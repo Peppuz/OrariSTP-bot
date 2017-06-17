7c39e48697a90bd17064ede862de96f1c1380890
Peppuz <peppuzvitale@gmail.com>
2017-06-17 11:11:35 +0200

    Add GNU LGPL + travis

--- SQL.py
+++ SQL.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# OrariSTP bot
+#This file is part of OrariSTP-bot.
 #
-# A Telegram Bot that provides times and stops for STP Brindisi
+# OrariSTP-bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OrariSTP-bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with OrariSTP-bot.  If not, see <http://www.gnu.org/licenses/>
 # 2016-2017
 # Peppuz Elativ <peppuzvitale@gmail.com>
 #
--- start_me.py
+++ start_me.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# OrariSTP bot
+#This file is part of OrariSTP-bot.
 #
-# A Telegram Bot that provides times and stops for STP Brindisi
+# OrariSTP-bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OrariSTP-bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with OrariSTP-bot.  If not, see <http://www.gnu.org/licenses/>
 # 2016-2017
 # Peppuz Elativ <peppuzvitale@gmail.com>
 #
 
-'''####################################################################################################################'''
-import telegram, logging, Tastiere, SQL, message_handler, callback_handler
+import telegram, logging, Tastiere, SQL, message_handler, callback_handler, credentials
 from telegram.ext import (Updater, MessageHandler, Filters, CommandHandler, CallbackQueryHandler)
 from telegram import (Emoji, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup)
 from telegram import ReplyKeyboardMarkup as RKM
-import credentials
 import update as reloaded
-'''####################################################################################################################'''
-# Settings
 
+# Settings
 bot = telegram.Bot(credentials.token)
 updater = Updater(credentials.token)
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%I:%M', level=logging.DEBUG)
 confirmed_id = credentials.confirmed_id
 
-'''####################################################################################################################'''
 # Defs
-
 def start(b,u):
     # meaning : start(bot,update_data)
     m 			= u.message
@@ -50,19 +56,17 @@ def vai(b,u):
 	bot.sendMessage(m.chat_id, text='Nuova ricerca! '+Emoji.BUS.decode('utf-8'), reply_markup=Tastiere.start())
 
 '''####################################################################################################################'''
+
+
 # Just Main
-
-
 def main():
 	ds = updater.dispatcher
-
 	ds.add_handler(CommandHandler('start',start))
 	ds.add_handler(CommandHandler('menu',cancel))
 	ds.add_handler(CommandHandler('vai', vai))
 	ds.add_handler(MessageHandler([Filters.text],message_handler.messagehandler))
 	ds.add_handler(CallbackQueryHandler(callback_handler.callbacking))
-
 	updater.start_polling()
-'''#-----------------------------------------------------'''
+
 if __name__ == '__main__':
 	main()
